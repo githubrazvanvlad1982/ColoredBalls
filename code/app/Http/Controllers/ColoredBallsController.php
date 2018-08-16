6a63c6b1bfddb262b769c9711fa17762e8ee6760
Razvan Vlad <razvan.vlad@elefant.ro>
2018-08-16 19:33:22 +0300

    ColoredBalls: add database conection

--- code/app/Http/Controllers/ColoredBallsController.php
+++ code/app/Http/Controllers/ColoredBallsController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Support\Facades\DB;
+
 class ColoredBallsController extends Controller
 {
     public function group()
     {
+        $users = DB::select('select * from test');
         return view('colored_balls');
     }
 }
